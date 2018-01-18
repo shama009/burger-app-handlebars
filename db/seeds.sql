3de6be7fd0d43997161ec3fc6ff0f12e54816dec
Shama <shamaanjum09@outlook.com>
2018-01-17 18:55:54 -0800

    setup the code to connect Node to MySQL

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
-INSERT INTO burgers (name) VALUES ('Chicken Cheese burger');
-INSERT INTO burgers (name) VALUES ('Hamburger');
-INSERT INTO burgers (name) VALUES ('Beef burger');
-INSERT INTO burgers (name) VALUES ('Veggie burger');
-INSERT INTO burgers (name) VALUES ('Double cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Beef burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie burger');
+INSERT INTO burgers (burger_name) VALUES ('Double cheese burger');
 
 Select * from burgers;
