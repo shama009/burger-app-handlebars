aba85cbe64bc8ec002cef2e28e938841cf3dbbda
Shama <shamaanjum09@outlook.com>
2018-01-17 18:50:18 -0800

    db set up

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name) VALUES ('Chicken Cheese burger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Beef burger');
+INSERT INTO burgers (name) VALUES ('Veggie burger');
+INSERT INTO burgers (name) VALUES ('Double cheese burger');
+
+Select * from burgers;
